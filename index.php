1ca99cccfca95dfc8b45b68caabf6304acfd65e2
Aruna-Sankari <arunaponrajkdm@gmail.com>
2024-03-29 12:27:22 +0530

    completed

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>Basics</title>
+</head>
+<body>
+    <?php
+    echo "<h1 align=center>Welcome To PHP Tutorial</h1>"
+    ?>
+</body>
+</html>
+
+<!-- Comments -->
+<?php
+// This is a single line comment 
+?>
+<?php
+/* This a 
+multi-line 
+comment 
+*/
+?>
